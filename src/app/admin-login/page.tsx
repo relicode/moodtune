5c6bb8803ab72792520961f132449f184f3de4e5
Anssi Siren <anssi@siren.codes>
2026-02-17 17:03:35 +0200

    Remove separate admin login page
    No longer needed now that the root page handles both roles.

--- src/app/admin-login/page.tsx
+++
@@ -1,6 +0,0 @@
-import { loginAdmin } from '$/actions/auth'
-import LoginForm from '$/components/LoginForm'
-
-const AdminLoginPage = () => <LoginForm title="Admin Login" action={loginAdmin} />
-
-export default AdminLoginPage
