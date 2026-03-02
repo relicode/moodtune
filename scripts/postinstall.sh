3935ddc691ba5bb4536a7922fd66d94def60134c
Anssi Siren <anssi@siren.codes>
2026-03-02 13:27:01 +0200

    Upgrade postinstall ffmpeg binaries from 7.1 to 8.0

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -5,7 +5,7 @@
 
 set -e
 
-FFMPEG_MAJOR="7.1"
+FFMPEG_MAJOR="8.0"
 BASE_URL="https://github.com/BtbN/FFmpeg-Builds/releases/download/latest"
 
 # Read DATA_DIR from .env if available (without sourcing the entire file for safety)
